2e42d890de026b231297426d0b20259f61f925e1
stan <47195572+staniski@users.noreply.github.com>
2021-07-19 13:27:19 +0100

    feat(project):add CRUD apis

--- web/project/__init__.py
+++ web/project/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from project.models import User, db
 
 
@@ -7,6 +7,75 @@ app.config.from_object("project.config.Config")
 db.init_app(app)
 
 
-@app.route("/")
-def hello_world():
-    return jsonify(hello="world")
+@app.route("/add", methods=["POST"])
+def add_user():
+    first_name = request.args.get('first_name')
+    last_name = request.args.get('last_name')
+    email = request.args.get('email')
+    phone_number = request.args.get('phone')
+    try:
+        user = User(
+            first_name=first_name,
+            last_name=last_name,
+            email=email,
+            phone_number=phone_number,
+        )
+        db.session.add(user)
+        db.session.commit()
+        return "User {} added".format(user.id)
+    except Exception as e:
+        return str(e)
+
+
+@app.route("/users", methods=["GET"])
+def get_all_users():
+    try:
+        users = User.query.all()
+        return jsonify([u.serialize() for u in users])
+    except Exception as e:
+        return str(e)
+
+
+@app.route("/users/<id_>", methods=["PUT"])
+def update_user_by_id(id_):
+
+    user = User.query.filter_by(id=id_).first()
+    if user:
+        first_name = request.args.get('first_name')
+        last_name = request.args.get('last_name')
+        email = request.args.get('email')
+        phone_number = request.args.get('phone')
+        try:
+            user.first_name = first_name
+            user.last_name = last_name
+            user.email = email
+            user.phone_number = phone_number
+            db.session.commit()
+            return "User {} was updated".format(user.id)
+        except Exception as e:
+            raise e
+    else:
+        raise FileNotFoundError
+
+
+@app.route("/users/<id_>")
+def get_user_by_id(id_):
+
+    user = User.query.filter_by(id=id_).first()
+    if user:
+        return jsonify(user.serialize())
+    else:
+        raise FileNotFoundError
+
+
+@app.route("/users/<id_>", methods=["DELETE"])
+def delete_user_by_id(id_):
+
+    user = User.query.filter_by(id=id_).first()
+    id = user.id
+    if user:
+        db.session.delete(user)
+        db.session.commit()
+        return "User {} was deleted".format(id)
+    else:
+        raise FileNotFoundError
--- web/project/models.py
+++ web/project/models.py
@@ -6,10 +6,10 @@ class User(db.Model):
     __tablename__ = "users"
 
     id = db.Column(db.Integer, primary_key=True)
-    first_name = db.Column(db.String(128), unique=False, nullable=False)
-    last_name = db.Column(db.String(128), default=False, nullable=False)
-    email = db.Column(db.String(128), unique=True, nullable=False)
-    phone_number = db.Column(db.String(128), unique=True, nullable=False)
+    first_name = db.Column(db.String(128), unique=False, nullable=True)
+    last_name = db.Column(db.String(128), default=False, nullable=True)
+    email = db.Column(db.String(128), unique=True, nullable=True)
+    phone_number = db.Column(db.String(128), unique=True, nullable=True)
 
     def __init__(self, first_name, last_name, email, phone_number):
         self.first_name = first_name
@@ -19,3 +19,12 @@ class User(db.Model):
 
     def __repr__(self):
         return '<id {}>'.format(self.id)
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'first_name': self.first_name,
+            'last_name': self.last_name,
+            'email': self.email,
+            'phone_number': self.phone_number
+        }
